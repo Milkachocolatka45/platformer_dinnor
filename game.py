3c5db976baba73f2d0d452796e0a6b4ff035a461
Milkachocolatka45 <plizunova5@gmail.com>
2025-02-16 13:25:43 +0200

    add class player and sprite i add walk player add images

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ pygame.init()
 
 level1_objects = draw_level(level1)
 
+player = Player(50, H - 90, 40, 50, 10, player_images)
+
 game = True 
 while game:
 
@@ -14,6 +16,9 @@ while game:
 
     window.blit(bg, (0, 0))
     level1_objects.draw(window)
+
+    player.update()
+    player.draw()
 
     pygame.display.update()
     clock.tick(FPS)
